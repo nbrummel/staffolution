ec341ebba631d6b0761e9f8cce7a762dbf22ff4d
Prelang Builder <builder@prelang.com>
2014-06-06 07:44:11 +0000

    Made model 'Employee' votable by adding acts_as_votable

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,4 +5,5 @@ class Employee < ActiveRecord::Base
   validates_formatting_of :employee_email, using: :email
   validates_formatting_of :employee_phone, using: :us_phone
   validates_formatting_of :employee_zipcode, using: :us_zip
+  acts_as_votable 
 end
