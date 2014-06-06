6b5b086e6d56097f843b031cd9de11e4eec89eaa
Prelang Builder <builder@prelang.com>
2014-06-06 07:43:48 +0000

    Defined validation for attribute 'rep_url' on model 'Reps'

--- app/models/reps.rb
+++ app/models/reps.rb
@@ -3,4 +3,5 @@ class Reps < ActiveRecord::Base
   has_many :education_classes
   validates_formatting_of :rep_email, using: :email
   validates_formatting_of :rep_phone, using: :us_phone
+  validates_formatting_of :rep_url, using: :url
 end
