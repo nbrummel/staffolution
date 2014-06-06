14df2ab4d90b023726f4d8df229e990035abde1f
Prelang Builder <builder@prelang.com>
2014-06-06 07:44:11 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Employee' to allow for create, update

--- app/admin/employee.rb
+++ app/admin/employee.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Employee do
+  permit_params :employee_email, :employee_phone, :employee_zipcode, :employee_first_name, :employee_last_name, :employee_bages, :employee_description, :employee_experience_years, :employee_experience_months, :employee_lic_number, :employee_type, :employee_rating, :employee_punctuality, :employee_dependability, :employee_reliable_transportation, :employee_accurate_calendar, :employee_transportation, :employee_school, :employee_languages, :employee_qualifications, :employee_credentials, :employee_experience, :employee_phone_verified, :employee_email_verified, :employee_lic_verified
 
   
   # See permitted parameters documentation:
