b88b259eef3a025601d56169d818018106701895
Prelang Builder <builder@prelang.com>
2014-06-06 07:43:47 +0000

    Defined validation for attribute 'rep_phone' on model 'Reps'

--- app/models/reps.rb
+++ app/models/reps.rb
@@ -2,4 +2,5 @@ class Reps < ActiveRecord::Base
   belongs_to :user
   has_many :education_classes
   validates_formatting_of :rep_email, using: :email
+  validates_formatting_of :rep_phone, using: :us_phone
 end
