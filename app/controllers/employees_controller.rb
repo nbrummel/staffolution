b44728f87089c239124799ba7f2a64ebb806b1eb
Prelang Builder <builder@prelang.com>
2014-06-06 07:44:13 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,7 +3,7 @@ class EmployeesController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_employee, only: [:show, :edit, :update, :destroy]
+  before_action :set_employee, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /employees
   # GET /employees.json
