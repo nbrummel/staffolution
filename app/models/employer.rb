80a81f78a42e5b3a642bf7d7e43e0c56aae4362a
Prelang Builder <builder@prelang.com>
2014-06-06 07:43:49 +0000

    Defined validation for attribute 'employer_email' on model 'Employer'

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -5,4 +5,5 @@ class Employer < ActiveRecord::Base
   validates_formatting_of :employer_zipcode, using: :us_zip
   validates_formatting_of :employer_phone, using: :us_phone
   validates_formatting_of :employer_url, using: :url
+  validates_formatting_of :employer_email, using: :email
 end
