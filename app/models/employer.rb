9a03b651ca18f6d1e853857b3624cce19d8872e9
Prelang Builder <builder@prelang.com>
2014-06-06 07:43:48 +0000

    Defined validation for attribute 'employer_phone' on model 'Employer'

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -3,4 +3,5 @@ class Employer < ActiveRecord::Base
   has_many :jobs
   has_many :reviews
   validates_formatting_of :employer_zipcode, using: :us_zip
+  validates_formatting_of :employer_phone, using: :us_phone
 end
